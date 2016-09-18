b208d959aed5dd39c6474ec1ff5d87f9ae2277da
chayaturner <dietaholic@gmail.com>
2016-09-18 00:28:46 -0400

    Change - display sum of two numbers

--- src/asgn01/Asgn01.java
+++ src/asgn01/Asgn01.java
@@ -1,9 +1,25 @@
 package asgn01;
 
+import java.util.Scanner;
+
 public class Asgn01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		int firstNum = 0;
+		int secondNum = 0;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter two numbers:");
+		System.out.println("Number 1: ");
+
+		firstNum = scanner.nextInt();
+
+		System.out.println("Number 2: ");
+		secondNum = scanner.nextInt();
+
+		int sum = firstNum + secondNum;
+		System.out.println("Sum: ");
+		System.out.println(sum);
 	}
 
 }
